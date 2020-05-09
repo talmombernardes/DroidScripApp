809d85696a136fc97c1739ec869e35f5206f478b
talmom bernardes <talmombernardes@gmail.com>
2020-05-09 16:57:52 -0300

    Add files via upload

---
+++ hourstoday.js
@@ -0,0 +1,61 @@
+// global
+var edt1
+var edt3
+//Called when application is started.
+function OnStart()
+{
+    //Create a layout
+    
+    lay = app.CreateLayout( "Linear", "VCenter,FillXY" );
+    lay.SetBackGradientRadial(0.5,0.5,1,"gray","black");
+    txt = app.CreateText( "Converter Hours in Days" );
+    lay.AddChild( txt );
+    
+    //Create 4 textEdits and add them to layout.
+
+    edt1 = app.CreateTextEdit("");
+    edt1.SetTextSize(17);    
+    edt1.SetHint("          ");
+    edt1.SetTextColor('white');
+    lay.AddChild( edt1 );
+   
+    
+    //--- Standard buttons ---
+  
+    layDark =  app.CreateLayout( "Linear", "Vertical" );
+    layDark.SetBackColor( "Alum" );
+    layDark.SetPadding( 0.1, 0.05, 0.1, 0.05 );
+    lay.AddChild( layDark );
+ 
+ 
+ 
+    //Create a "Calculate" button
+    
+    var btn = app.CreateButton("Calculate",  0.7, -1,"Alum");
+    btn.SetOnTouch(btnOnTouch);
+    btn.SetTextColor('black');
+    layDark.AddChild(btn);
+    
+    //Add layout to app.
+    
+    app.AddLayout( lay );
+}
+ 
+function btnOnTouch()
+{
+    const edit2 = 24
+    const res = parseInt(edt1)
+        / edit2
+
+
+app.Alert('Total hours in Days: ' + Math.trunc(res)+ ' Days');
+}
+
+function parseInt(control) 
+{
+//take a control (probably a TextEdit) and return its text as a number
+    var value = control.GetText();
+    var ret = +(value.replace(',','.'));
+    if( isNaN(ret) ) app.ShowPopup(value +" is not a number");
+    return ret;
+}
